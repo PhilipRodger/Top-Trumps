8285fa2e0700aea60dcfacac57c95703ce139867
Berlian Khatulistiwa <berlink.k2@gmail.com>
2019-01-30 01:43:28 +0000

    Implemented rest of database related classes.

--- src/model/Database.java
+++ src/model/Database.java
@@ -3,20 +3,21 @@ package model;
 import java.sql.*;
 
 public class Database {
- 
-	public Database() { //default constructor
-		
+	private Connection c = null; // connection attribute for connecting to database
+
+	public Database() { // default constructor
+
 	}
+
 	/*
-	 * Method that makes connection to database. Makes print statement to console to report
-	 * connection status.
+	 * Method that makes connection to database. Makes print statement to console to
+	 * report connection status.
 	 * 
 	 */
 	public Connection connectToDB() {
 		final String username = "postgres";
 		final String password = "6114680d";
 		final String dbName = "jdbc:postgresql:topTrumps";
-		Connection c = null;
 
 		try {
 			c = DriverManager.getConnection(dbName, username, password);
@@ -34,6 +35,19 @@ public class Database {
 	}
 
 	/**
+	 * Severs connection to database
+	 */
+	public void disconnectDB() {
+		try {
+			c.close();
+
+		} catch (SQLException e) {
+			System.out.println("You could not disconnect from the database.");
+			e.printStackTrace();
+		}
+	}
+
+	/**
 	 * Writes to the database the number of draws, who won the game, number of
 	 * rounds in the game and how many rounds each player won.
 	 *
@@ -43,12 +57,14 @@ public class Database {
 	public void writeGameStatistics(GameStatistics gstats) {
 		Player[] players = gstats.getPlayerArray();
 		Player winner = gstats.getWinner();
-		
-		int gameID = 1; // game stats to be added to database each game
-		int nDraws = gstats.getNumOfDraws(); //number of draws in the game
-		int nRounds = gstats.getNumOfRounds(); //number of rounds in the game
-		int gWinner = 0; 						//integer denoting which player won the game
-		int p1RW = players[0].getRoundsWon(); //rounds won per player
+
+		int gameID = 0; // game stats to be added to database each game, first game in database has
+						// gameID of 0
+		int nDraws = gstats.getNumOfDraws(); // number of draws in the game
+		int nRounds = gstats.getNumOfRounds(); // number of rounds in the game
+		int gWinner = 0; // integer denoting which player won the game. if 1, human player wins, beyond 1
+							// denotes a computer victory
+		int p1RW = players[0].getRoundsWon(); // rounds won per player
 		int p2RW = players[1].getRoundsWon();
 		int p3RW = players[2].getRoundsWon();
 		int p4RW = players[3].getRoundsWon();
@@ -66,32 +82,153 @@ public class Database {
 				gWinner = 5;
 			}
 		}
-		String gameStats = "INSERT INTO TopTrumpStats VALUES (" + gameID + ", " + nDraws + ", " + nRounds + ", "
-				+ gWinner + ", " + p1RW + ", " + p2RW + ", " + p3RW + ", " + p4RW + ", " + p5RW + ");";
+		String gameStats = "INSERT INTO Game VALUES (" + gameID + ", " + nDraws + ", " + nRounds + ", " + gWinner + ", "
+				+ p1RW + ", " + p2RW + ", " + p3RW + ", " + p4RW + ", " + p5RW + ");";
 
 		insertStats(gameStats);
 		gameID++;
 	}
+
 	/**
-	 * takes a string of game stats from the method writeGameStatistics()
-	 * and uses the string to insert game stats into the database
+	 * takes a string of game stats from the method writeGameStatistics() and uses
+	 * the string to insert game stats into the database
+	 * 
 	 * @param stats
 	 */
-	
+
 	public void insertStats(String stats) {
 		Statement stmt = null;
-		Connection c = connectToDB();
 		if (c != null) {
 			try {
 				stmt = c.createStatement();
 				stmt.executeQuery(stats);
 				stmt.close();
-			
-			}catch (SQLException e) {
+
+			} catch (SQLException e) {
 				e.printStackTrace();
 			}
-			
+
 		}
+	}
+
+	/**
+	 * Queries database to get total number of games played.
+	 * 
+	 * @return total number of games played
+	 */
+	public int getTotalGamesPlayed() {
+		Statement stmt = null;
+		String sql = "Select Count(gameID) as totalGames From Game";
+		int totalGamesPlayed = 0;
+
+		try {
+			stmt = c.createStatement();
+			ResultSet results = stmt.executeQuery(sql);
+			while (results.next()) {
+				totalGamesPlayed = results.getInt("totalGames");
+			}
+
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return totalGamesPlayed;
+
+	}
+
+	/**
+	 * Queries database to get total computer wins.
+	 * 
+	 * @return total number of games played
+	 */
+	public int getTotalComputerWins() {
+		Statement stmt = null;
+		String sql = "Select Count(game_winner) as computerWins From Game Where game_winner > 1 ";
+		int totalComputerWins = 0;
+
+		try {
+			stmt = c.createStatement();
+			ResultSet results = stmt.executeQuery(sql);
+			while (results.next()) {
+				totalComputerWins = results.getInt("computerWins");
+			}
+
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return totalComputerWins;
+
+	}
+
+	/**
+	 * Queries database to get total human wins.
+	 * 
+	 * @return total number of games played
+	 */
+	public int getTotalHumanWins() {
+		Statement stmt = null;
+		String sql = "SELECT COUNT(game_winner) as humanWins FROM Game WHERE game_winner = 1 ";
+		int totalHumanWins = 0;
+
+		try {
+			stmt = c.createStatement();
+			ResultSet results = stmt.executeQuery(sql);
+			while (results.next()) {
+				totalHumanWins = results.getInt("humanWins");
+			}
+
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return totalHumanWins;
+
+	}
+
+	/**
+	 * Queries database to get average draws per game.
+	 * 
+	 * @return total number of games played
+	 */
+	public int getAverageDrawsPerGame() {
+		Statement stmt = null;
+		String sql = "SELECT AVG(total_draws) as avgDraws FROM Game ";
+		int averageDraws = 0;
+
+		try {
+			stmt = c.createStatement();
+			ResultSet results = stmt.executeQuery(sql);
+			while (results.next()) {
+				averageDraws = results.getInt("avgDraws");
+			}
+
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return averageDraws;
+
+	}
+
+	/**
+	 * Queries database to get largest number of rounds played in a game.
+	 * 
+	 * @return total number of games played
+	 */
+	public int getLargestNumberOfRounds() {
+		Statement stmt = null;
+		String sql = "SELECT MAX(total_rounds) as maxTotalRounds FROM Game ";
+		int largestNumberofRounds = 0;
+
+		try {
+			stmt = c.createStatement();
+			ResultSet results = stmt.executeQuery(sql);
+			while (results.next()) {
+				largestNumberofRounds = results.getInt("maxTotalRounds");
+			}
+
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return largestNumberofRounds;
+
 	}
 
 	/**
@@ -102,8 +239,17 @@ public class Database {
 	 */
 	public DatabaseResponse getDatabaseStats() {
 		DatabaseResponse response = new DatabaseResponse();
+		int totalGamesPlayed = getTotalGamesPlayed();
+		int totalComputerWins = getTotalComputerWins();
+		int totalHumanWins = getTotalHumanWins();
+		int averageDrawsPerGame = getAverageDrawsPerGame();
+		int largestNumberOfRounds = getLargestNumberOfRounds();
 
-		// TODO make queries to the database and update the response with that info.
+		response.setTotalGamesPlayed(totalGamesPlayed);
+		response.setTotalComputerWins(totalComputerWins);
+		response.setTotalHumanWins(totalHumanWins);
+		response.setAverageDrawsPerGame(averageDrawsPerGame);
+		response.setLargestNumberOfRounds(largestNumberOfRounds);
 
 		return response;
 	}
--- src/model/DatabaseResponse.java
+++ src/model/DatabaseResponse.java
@@ -6,38 +6,51 @@ public class DatabaseResponse {
 	private int totalHumanWins;
 	private int averageDrawsPerGame;
 	private int largestNumberOfRounds;
-	
-	
+
 	public int getTotalGamesPlayed() {
 		return totalGamesPlayed;
 	}
+
 	public void setTotalGamesPlayed(int totalGamesPlayed) {
 		this.totalGamesPlayed = totalGamesPlayed;
 	}
+
 	public int getTotalComputerWins() {
 		return totalComputerWins;
 	}
+
 	public void setTotalComputerWins(int totalComputerWins) {
 		this.totalComputerWins = totalComputerWins;
 	}
+
 	public int getTotalHumanWins() {
 		return totalHumanWins;
 	}
-	
-	
+
 	public void setTotalHumanWins(int totalHumanWins) {
 		this.totalHumanWins = totalHumanWins;
 	}
+
 	public int getAverageDrawsPerGame() {
 		return averageDrawsPerGame;
 	}
+
 	public void setAverageDrawsPerGame(int averageDrawsPerGame) {
 		this.averageDrawsPerGame = averageDrawsPerGame;
 	}
+
 	public int getLargestNumberOfRounds() {
 		return largestNumberOfRounds;
 	}
+
 	public void setLargestNumberOfRounds(int largestNumberOfRounds) {
 		this.largestNumberOfRounds = largestNumberOfRounds;
 	}
+
+	public String toString() {
+		return "Total games played= " + totalGamesPlayed + ", Total Computer Wins= " + totalComputerWins
+				+ ", Total Human Wins= " + totalHumanWins + ", Acerage Draws per Game = " + averageDrawsPerGame
+				+ ", Largest Number of Rounds = " + largestNumberOfRounds + "]";
+	}
+
 }
